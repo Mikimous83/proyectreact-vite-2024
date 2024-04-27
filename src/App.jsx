e20954466179d5fe5103eb81dca3cc14c745d2a9
Mikimous83 <chatgpt.mouse@gmail.com>
2024-04-26 20:59:23 -0500

    segundo comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import './App.css'
+
 function App() {
+    const API_URL = import.meta.env.VITE_API_URL
+    const [criptos, setCriptos] = useState()
 
-  return (
-         <h1>HOLA MUNDO</h1>
+    useEffect(() => {
+        axios.get(`${API_URL}assets`)
+        // fetch(`${API_URL}assets`)
+            // .then(response => response.json())
+            .then(data => {
+                // console.log(data)
+                setCriptos(data.data.data)
+            })
+            .catch(() => {
+                console.error('la peticion fallo')
+            })
+    }, [])
+    if (!criptos) return <span>Cargando...</span>
 
-  )
+    return (
+        <>
+            <h1>Lista de criptomonedas</h1>
+            <ol>
+                {
+                    criptos.map(({id,name, priceUsd}) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <li key={id}>Nombre: {name} Precio: {priceUsd} </li>
+                    ))
+                }
+            </ol>
+        </>
+    )
 }
 
 export default App
